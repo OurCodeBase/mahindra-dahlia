9a36b68aa04700c7034836b0b2f8381e63312c39
hαrsh vαirαgi <harshb7839@gmail.com>
2025-10-24 19:32:48 +0000

    extension initialised without user interface.

---
+++ manifest.config.ts
@@ -0,0 +1,29 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+
+export default defineManifest({
+  manifest_version: 3,
+  name: "mahindra-dahlia",
+  description: "A chromium extension to handle initial follow up by itself.",
+  version: "1.0.0",
+  icons: {
+    16: "icon.png",
+    32: "icon.png",
+    48: "icon.png",
+    128: "icon.png"
+  },
+  action: {
+    default_title: "Click here to handle initial follow up.",
+  },
+  background: {
+    service_worker: "src/background.ts",
+    type: "module",
+  },
+  permissions: [
+    "scripting",
+    "activeTab"
+  ],
+  host_permissions: [
+    "*://localhost:*/*",
+    "*://mahindra.my.site.com/*"
+  ]
+});
